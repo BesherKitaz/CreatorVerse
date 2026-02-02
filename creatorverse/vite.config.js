8d3cdb0fbcdcd687a1f2780e7b39fb668b99ac29
Besher Kitaz <kitazb@berea.edu>
2026-02-01 22:48:40 -0500

    Final distro inshallah!

--- creatorverse/vite.config.js
+++ creatorverse/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'docs', // <-- change "dist" to "build" or whatever you like
+    outDir: '../docs', // <-- change "dist" to "build" or whatever you like
   },
   plugins: [react()],
   base: "/CreatorVerse/",
