7e18b69fe5312da4174935077e4f47686d7d2004
Besher Kitaz <kitazb@berea.edu>
2026-02-01 23:03:15 -0500

    final dist fr fr

--- creatorverse/src/App.jsx
+++ creatorverse/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Creators />} />
           <Route path="/add-creator" element={<AddCreator />} />
